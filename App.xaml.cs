83c0e38f5d42cbdcc439c589388fcfe806576a0a
bashergoingcrazy <anjusareene4@gmail.com>
2023-08-15 20:47:06 +0530

    On the path of creating a system tray Icon

---
+++ App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace myWallpaperCarousel
+{
+    public partial class App : Application
+    {
+        private TaskbarIcon tb = null!;
+
+        private void InitApplication()
+        {
+            tb = (TaskbarIcon)FindResource("SystemTrayIcon");
+        }
+    }
+}
